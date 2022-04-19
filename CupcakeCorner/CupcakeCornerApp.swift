864eb186831d483e989f1ec15efde36e5f14e2be
Yasin <yasinerdemli35@icloud.com>
2022-04-19 11:06:05 +0300

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Yasin Erdemli on 19.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
